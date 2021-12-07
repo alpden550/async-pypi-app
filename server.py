a49c6145d579da751f240e5d7c0fd3f80aaeeb98
Denis Novikov <alpden550@gmail.com>
2021-12-07 18:24:03 +0300

    feat(server): add routers

---
+++ server.py
@@ -0,0 +1,32 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi_chameleon import global_init
+
+from pypi.views import accounts, home, packages
+
+app = FastAPI()
+
+
+def configure_templates():
+    global_init(template_folder="pypi/templates", auto_reload=True)
+
+
+def configure_routes():
+    app.include_router(home.router)
+    app.include_router(accounts.router)
+    app.include_router(packages.router)
+
+
+def configure_app():
+    configure_templates()
+    configure_routes()
+
+
+def main():
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    configure_app()
