a7f485b3b6e9f6ebfcc12b5994bd820b0a0a5d82
Denis Novikov <alpden550@gmail.com>
2021-12-12 12:29:41 +0300

    feat(views): add models for accounts views

--- pypi/views/accounts.py
+++ pypi/views/accounts.py
@@ -1,27 +1,33 @@
 from fastapi import APIRouter
+from starlette.requests import Request
+
+from pypi.models.account import AccountViewModel, LoginViewModel, RegisterViewModel
 
 router = APIRouter()
 
 
 @router.get("/account")
-def account():
+def account(request: Request):
     """."""
-    return {}
+    account_model = AccountViewModel(request)
+    return account_model.to_dict()
 
 
 @router.get("/account/register")
-def register():
+def register(request: Request):
     """."""
-    return {}
+    register_model = RegisterViewModel(request)
+    return register_model.to_dict()
 
 
 @router.get("/account/login")
-def login():
+def login(request: Request):
     """."""
-    return {}
+    login_model = LoginViewModel(request)
+    return login_model.to_dict()
 
 
 @router.get("/account/logout")
-def logout():
+def logout(request: Request):
     """."""
     return {}
