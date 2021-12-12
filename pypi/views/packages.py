5641ed416a9b3c0a0ed623a66b38bf8ba26c31c9
Denis Novikov <alpden550@gmail.com>
2021-12-12 12:30:52 +0300

    feat(views): add model for package page

--- pypi/views/packages.py
+++ pypi/views/packages.py
@@ -1,3 +1,14 @@
 from fastapi import APIRouter
+from fastapi_chameleon import template
+from starlette.requests import Request
+
+from pypi.models.packages import DetailPackageViewModel
 
 router = APIRouter()
+
+
+@router.get("/{package_name}")
+@template("packages/details.pt")
+def detail(package_name: str, request: Request):
+    detail_model = DetailPackageViewModel(package_name, request)
+    return detail_model.to_dict()
