f43fa5181cdd77a54bd8c66edac3304d4b5926c3
Denis Novikov <alpden550@gmail.com>
2021-12-18 12:14:46 +0300

    feat(models): update base view model about getting from cookies

--- pypi/models/shared/view_model.py
+++ pypi/models/shared/view_model.py
@@ -2,13 +2,15 @@ from typing import Any, Optional
 
 from starlette.requests import Request
 
+from pypi.infrastructure import cookie_auth
+
 
 class BaseViewModel:
     def __init__(self, request: Request):
         self.request: Request = request
         self.error: Optional[str] = None
         self.user_id: Optional[id] = None
-        self.is_logged_in = False
+        self.is_logged_in = cookie_auth.get_user_id_from_cookie(self.request)
 
     def to_dict(self) -> dict[str, Any]:
         return self.__dict__
