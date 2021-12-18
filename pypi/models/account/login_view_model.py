8bfca7e34f551d1453088a13655258b8d5abf8d4
Denis Novikov <alpden550@gmail.com>
2021-12-18 12:14:05 +0300

    feat(models): add login view model

--- pypi/models/account/login_view_model.py
+++ pypi/models/account/login_view_model.py
@@ -1,5 +1,23 @@
+from typing import Optional
+
+from starlette.requests import Request
+
 from pypi.models.shared.view_model import BaseViewModel
 
 
 class LoginViewModel(BaseViewModel):
-    pass
+    def __init__(self, request: Request):
+        super().__init__(request)
+
+        self.email: Optional[str] = None
+        self.password: Optional[str] = None
+
+    async def load(self):
+        form = await self.request.form()
+        self.email = form.get("email").lower().strip()
+        self.password = form.get("password").strip()
+
+        if not self.email:
+            self.error = "You must specify the email."
+        elif not self.password:
+            self.error = "you must specify the password."
