0a35277daa54b10f2ddb30b8b0c17596978aab97
Denis Novikov <alpden550@gmail.com>
2021-12-18 12:14:12 +0300

    feat(models): add register view model

--- pypi/models/account/register_view_model.py
+++ pypi/models/account/register_view_model.py
@@ -1,5 +1,27 @@
+from typing import Optional
+
+from starlette.requests import Request
+
 from pypi.models.shared.view_model import BaseViewModel
 
 
 class RegisterViewModel(BaseViewModel):
-    pass
+    def __init__(self, request: Request):
+        super().__init__(request)
+
+        self.name: Optional[str] = None
+        self.email: Optional[str] = None
+        self.password: Optional[str] = None
+
+    async def load(self):
+        form = await self.request.form()
+        self.name = form.get("name")
+        self.email = form.get("email")
+        self.password = form.get("password")
+
+        if not self.name or not self.name.strip():
+            self.error = "Your name is required."
+        elif not self.email or not self.email.strip():
+            self.error = "Your email is required."
+        elif not self.password or len(self.password) < 5:
+            self.error = "Your password is required and must be at 5 characters."
