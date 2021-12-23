85645598abf77ba6ad616035d364bb6da9e74d58
Denis Novikov <alpden550@gmail.com>
2021-12-23 18:56:12 +0300

    feat(config): add db url

--- pypi/config.py
+++ pypi/config.py
@@ -1,11 +1,12 @@
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
     auth_cookie_name: str
     salt: str
+    db_url: str = Field(..., env="DATABASE_URL")
 
     class Config:
         env_file = ".env"
