56a26d9240018ed59b12d547417661e9d841b282
Denis Novikov <alpden550@gmail.com>
2021-12-23 18:57:21 +0300

    feat(data): add user model

---
+++ pypi/data/user.py
@@ -0,0 +1,17 @@
+import datetime
+
+import sqlalchemy as sa
+
+from pypi.data.base_model import BaseSQLAlchemy
+
+
+class User(BaseSQLAlchemy):
+    __tablename__ = "users"
+
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name: str = sa.Column(sa.String)
+    email: str = sa.Column(sa.String, index=True, unique=True)
+    hash_password: str = sa.Column(sa.String)
+    created_date: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    last_login: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    profile_image_url: str = sa.Column(sa.String)
