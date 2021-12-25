637bbafce2152f0aff6d264d524eeb7c77a6997f
Denis Novikov <alpden550@gmail.com>
2021-12-25 16:48:53 +0300

    feat(models): add repr for user model

--- pypi/data/models/user.py
+++ pypi/data/models/user.py
@@ -11,8 +11,11 @@ class User(BaseSQLAlchemy):
     id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name: str = sa.Column(sa.String, index=True)
     username: str = sa.Column(sa.String, nullable=True)
-    email: str = sa.Column(sa.String, index=True, unique=True)
+    email: str = sa.Column(sa.String, index=True, unique=True, nullable=True)
     hash_password: str = sa.Column(sa.String)
     created_at: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
     last_login_at: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
     profile_image_url: str = sa.Column(sa.String)
+
+    def __repr__(self):
+        return f"User {self.name} – {self.email}"
