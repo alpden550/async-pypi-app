6cac1c04d2b12fcede772c3608e7a8dc7eb2a864
Denis Novikov <alpden550@gmail.com>
2021-12-26 14:05:28 +0300

    feat(view models): update base view model

--- pypi/view_models/shared/view_model.py
+++ pypi/view_models/shared/view_model.py
@@ -9,8 +9,8 @@ class BaseViewModel:
     def __init__(self, request: Request):
         self.request: Request = request
         self.error: Optional[str] = None
-        self.user_id: Optional[id] = None
-        self.is_logged_in = cookie_auth.get_user_id_from_cookie(self.request)
+        self.user_id: Optional[id] = cookie_auth.get_user_id_from_cookie(self.request)
+        self.is_logged_in = self.user_id is not None
 
     def to_dict(self) -> dict[str, Any]:
         return self.__dict__
