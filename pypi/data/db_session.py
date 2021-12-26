76fa4c50d1a51b647d98b74fbcc18f73acd59eca
Denis Novikov <alpden550@gmail.com>
2021-12-26 16:40:09 +0300

    feat(db): add async create session

--- pypi/data/db_session.py
+++ pypi/data/db_session.py
@@ -2,17 +2,20 @@ from typing import Optional, Callable
 
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
 from sqlalchemy.orm import Session
 
 from pypi.config import get_settings
 from pypi.data.models.base_model import BaseSQLAlchemy
 
 __factory: Optional[Callable[[], Session]] = None
-engine = sa.create_engine(get_settings().database_url, pool_pre_ping=True, echo=False)
+__async_engine: Optional[AsyncEngine] = None
+engine: Engine = sa.create_engine(get_settings().database_url, pool_pre_ping=True, echo=False)
 
 
 def global_init(db_file: str):
-    global __factory
+    global __factory, __async_engine
 
     if __factory:
         return
@@ -21,6 +24,7 @@ def global_init(db_file: str):
         raise Exception("You must specify the database.")
 
     __factory = orm.sessionmaker(bind=engine)
+    __async_engine = create_async_engine(get_settings().async_database_url, echo=False)
 
     import pypi.data.models.__all_models  # noqa
 
@@ -35,5 +39,17 @@ def create_session() -> Session:
 
     session: Session = __factory()
     session.expire_on_commit = False
+
+    return session
+
+
+def create_async_session() -> AsyncSession:
+    global __async_engine
+
+    if not __async_engine:
+        raise Exception("You must call global_init() before using this method.")
+
+    session: AsyncSession = AsyncSession(__async_engine)
+    session.sync_session.expire_on_commit = False
 
     return session
