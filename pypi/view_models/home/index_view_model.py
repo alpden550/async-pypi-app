482f40442c02d878b800008c207f421bfc785cb8
Denis Novikov <alpden550@gmail.com>
2021-12-26 16:39:30 +0300

    chore(view models): make index view model async

--- pypi/view_models/home/index_view_model.py
+++ pypi/view_models/home/index_view_model.py
@@ -1,14 +1,20 @@
 from starlette.requests import Request
 
-from pypi.view_models.shared import BaseViewModel
 from pypi.services import package_service, user_service
+from pypi.view_models.shared import BaseViewModel
 
 
 class IndexViewModel(BaseViewModel):
     def __init__(self, request: Request):
         super().__init__(request)
 
-        self.package_count: int = package_service.count_packages()
-        self.release_count: int = package_service.count_releases()
-        self.user_count: int = user_service.user_count()
-        self.packages: list[dict[str, str]] = package_service.fetch_latest_releases(limit=5)
+        self.package_count: int = 0
+        self.release_count: int = 0
+        self.user_count: int = 0
+        self.packages: list[dict[str, str]] = []
+
+    async def load(self):
+        self.package_count: int = await package_service.count_packages()
+        self.release_count: int = await package_service.count_releases()
+        self.user_count: int = await user_service.user_count()
+        self.packages: list[dict[str, str]] = await package_service.fetch_latest_releases(limit=5)
