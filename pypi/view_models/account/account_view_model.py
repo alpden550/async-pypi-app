f92f650788427776d85beb99c5460ee037ec56bc
Denis Novikov <alpden550@gmail.com>
2021-12-26 16:39:01 +0300

    chore(view models): make account view model async

--- pypi/view_models/account/account_view_model.py
+++ pypi/view_models/account/account_view_model.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from starlette.requests import Request
 
+from pypi.data.models.user import User
 from pypi.services import user_service
 from pypi.view_models.shared.view_model import BaseViewModel
 
@@ -7,4 +10,7 @@ from pypi.view_models.shared.view_model import BaseViewModel
 class AccountViewModel(BaseViewModel):
     def __init__(self, request: Request):
         super().__init__(request)
-        self.user = user_service.get_user_by_id(self.user_id)
+        self.user: Optional[User] = None
+
+    async def load(self):
+        self.user = await user_service.get_user_by_id(self.user_id)
