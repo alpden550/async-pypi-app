c5d77e379d93a2139e10d1aa177417635e116373
Denis Novikov <alpden550@gmail.com>
2021-12-26 14:05:10 +0300

    feat(view models): add getting user for account view model

--- pypi/view_models/account/account_view_model.py
+++ pypi/view_models/account/account_view_model.py
@@ -1,10 +1,10 @@
 from starlette.requests import Request
 
-from pypi.data.user import User
+from pypi.services import user_service
 from pypi.view_models.shared.view_model import BaseViewModel
 
 
 class AccountViewModel(BaseViewModel):
     def __init__(self, request: Request):
         super().__init__(request)
-        self.user = User("Denis", "alpden@me.com", "password")
+        self.user = user_service.get_user_by_id(self.user_id)
