6d1e6cce8884b6eef9dc1e8c92937d34f5f0f4b0
Denis Novikov <alpden550@gmail.com>
2021-12-26 16:40:35 +0300

    feat(view models): add detail view model as async

--- pypi/view_models/packages/detail_view_model.py
+++ pypi/view_models/packages/detail_view_model.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
 from starlette.requests import Request
 
-from pypi.view_models.shared.view_model import BaseViewModel
+from pypi.data.models.package import Package
+from pypi.data.models.release import Release
 from pypi.services import package_service
+from pypi.view_models.shared.view_model import BaseViewModel
 
 
 class DetailPackageViewModel(BaseViewModel):
@@ -9,12 +13,15 @@ class DetailPackageViewModel(BaseViewModel):
         super().__init__(request)
 
         self.package_name = package_name
-        self.package = package_service.get_package(package_name)
-        self.latest_release = package_service.get_latest_package_release(package_name)
+        self.package: Optional[Package] = None
+        self.latest_release: Optional[Release] = None
         self.latest_version = "0.0.0"
         self.is_latest = True
         self.maintainers = []
 
+    async def load(self):
+        self.package = await package_service.get_package(self.package_name)
+        self.latest_release = await package_service.get_latest_package_release(self.package_name)
         if not self.package or not self.latest_release:
             return
 
