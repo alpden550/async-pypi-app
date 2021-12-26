2a3082290be427ce63e9a53f348c712809e696d9
Denis Novikov <alpden550@gmail.com>
2021-12-26 16:39:54 +0300

    feat(config): add async database url

--- pypi/config.py
+++ pypi/config.py
@@ -1,12 +1,13 @@
 from functools import lru_cache
 
-from pydantic import BaseSettings, PostgresDsn
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     auth_cookie_name: str
     salt: str
-    database_url: PostgresDsn
+    database_url: str
+    async_database_url: str
 
     class Config:
         env_file = ".env"
