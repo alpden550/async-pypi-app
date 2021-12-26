bc2c1fc3aac6463511f42c3211d7ab222159f5b4
Denis Novikov <alpden550@gmail.com>
2021-12-26 16:41:23 +0300

    feat(services): add async package services

--- pypi/services/package_service.py
+++ pypi/services/package_service.py
@@ -1,35 +1,50 @@
-from datetime import datetime
 from typing import Optional
 
+import sqlalchemy as sa
+from sqlalchemy import func
+from sqlalchemy.engine import ChunkedIteratorResult
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from pypi.data import db_session
 from pypi.data.models.package import Package
 from pypi.data.models.release import Release
 
 
-def count_packages() -> int:
-    return 0
+async def count_packages() -> int:
+    session: AsyncSession
+    async with db_session.create_async_session() as session:
+        result: ChunkedIteratorResult = await session.execute(sa.select(func.count(Package.id)))
+        return result.scalar()
 
 
-def count_releases() -> int:
-    return 0
+async def count_releases() -> int:
+    session: AsyncSession
+    async with db_session.create_async_session() as session:
+        result: ChunkedIteratorResult = await session.execute(sa.select(func.count(Release.id)))
+        return result.scalar()
 
 
-def fetch_latest_releases(limit: int = 5) -> list:
-    return [
-        {"id": "fastapi", "summary": "Async web framework"},
-        {"id": "uvicorn", "summary": "ASI server"},
-    ][:limit]
+async def fetch_latest_releases(limit: int = 5) -> list[Package]:
+    session: AsyncSession
+    async with db_session.create_async_session() as session:
+        query = sa.select(Release).order_by(Release.created_at.desc()).limit(limit)  # noqa
+        result: ChunkedIteratorResult = await session.execute(query)
+        releases = result.scalars()
+
+    return [release.package for release in releases]
 
 
-def get_package(package_name: str) -> Optional[Package]:
-    return Package(
-        package_name=package_name,
-        summary="This is the summary",
-        description="Full detail here",
-        home_page="www.google.com",
-        package_license="MIT",
-        author_name="Sebastian",
-    )
+async def get_package(package_name: str) -> Optional[Package]:
+    session: AsyncSession
+    async with db_session.create_async_session() as session:
+        query = sa.select(Package).filter_by(id=package_name)
+        result: ChunkedIteratorResult = await session.execute(query)
+        return result.scalar_one_or_none()
 
 
-def get_latest_package_release(package_name: str) -> Optional[Release]:
-    return Release("1.2.3", datetime.now())
+async def get_latest_package_release(package_name: str) -> Optional[Release]:
+    session: AsyncSession
+    async with db_session.create_async_session() as session:
+        query = sa.select(Release).filter_by(package_id=package_name).order_by(Release.created_at.desc())  # noqa
+        result: ChunkedIteratorResult = await session.execute(query)
+        return result.scalar()
