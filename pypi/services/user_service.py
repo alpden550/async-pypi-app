416dc15bc11b2239d1d65c814ab00cad09e01a04
Denis Novikov <alpden550@gmail.com>
2021-12-27 10:26:49 +0300

    refact(services): add async session as depends for user

--- pypi/services/user_service.py
+++ pypi/services/user_service.py
@@ -6,49 +6,38 @@ from sqlalchemy import func
 from sqlalchemy.engine import ChunkedIteratorResult
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from pypi.data import db_session
 from pypi.data.models.user import User
 
 
-async def user_count() -> int:
-    session: AsyncSession
-    async with db_session.create_async_session() as session:
-        result: ChunkedIteratorResult = await session.execute(sa.select(func.count(User.id)))
-        return result.scalar()
+async def user_count(session: AsyncSession) -> int:
+    result: ChunkedIteratorResult = await session.execute(sa.select(func.count(User.id)))
+    return result.scalar()
 
 
-async def create_account(name: str, email: str, password: str) -> User:
-    session: AsyncSession
-    async with db_session.create_async_session() as session:
-        user = User(name=name, email=email, hash_password=crypto.hash(password, rounds=172_434))
-        session.add(user)
-        await session.commit()
+async def create_account(session: AsyncSession, name: str, email: str, password: str) -> User:
+    user = User(name=name, email=email, hash_password=crypto.hash(password, rounds=172_434))
+    session.add(user)
+    await session.commit()
     return user
 
 
-async def login_user(email: str, password: str) -> Optional[User]:
-    session: AsyncSession
-    async with db_session.create_async_session() as session:
-        query = sa.select(User).filter_by(email=email)
-        result: ChunkedIteratorResult = await session.execute(query)
-        user = result.scalar_one_or_none()
+async def login_user(session: AsyncSession, email: str, password: str) -> Optional[User]:
+    query = sa.select(User).filter_by(email=email)
+    result: ChunkedIteratorResult = await session.execute(query)
+    user = result.scalar_one_or_none()
 
-        if not user or not crypto.verify(password, user.hash_password):
-            return None
-        return user
+    if not user or not crypto.verify(password, user.hash_password):
+        return None
+    return user
 
 
-async def get_user_by_id(user_id: int) -> Optional[User]:
-    session: AsyncSession
-    async with db_session.create_async_session() as session:
-        query = sa.select(User).filter_by(id=user_id)
-        result: ChunkedIteratorResult = await session.execute(query)
-        return result.scalar_one_or_none()
+async def get_user_by_id(session: AsyncSession, user_id: int) -> Optional[User]:
+    query = sa.select(User).filter_by(id=user_id)
+    result: ChunkedIteratorResult = await session.execute(query)
+    return result.scalar_one_or_none()
 
 
-async def get_user_by_email(email: str) -> Optional[User]:
-    session: AsyncSession
-    async with db_session.create_async_session() as session:
-        query = sa.select(User).filter_by(email=email)
-        result: ChunkedIteratorResult = await session.execute(query)
-        return result.scalar_one_or_none()
+async def get_user_by_email(session: AsyncSession, email: str) -> Optional[User]:
+    query = sa.select(User).filter_by(email=email)
+    result: ChunkedIteratorResult = await session.execute(query)
+    return result.scalar_one_or_none()
