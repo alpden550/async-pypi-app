1e2da86a78a879e9b86917b2b5921f9315ee433c
Denis Novikov <alpden550@gmail.com>
2021-12-27 10:25:06 +0300

    refact(views): add async session as depends for package

--- pypi/views/packages.py
+++ pypi/views/packages.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi_chameleon import template
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 
+from pypi.data.db_session import get_session
 from pypi.view_models.packages import DetailPackageViewModel
 
 router = APIRouter()
@@ -9,7 +11,7 @@ router = APIRouter()
 
 @router.get("/{package_name}")
 @template("packages/details.pt")
-async def detail(package_name: str, request: Request):
+async def detail(package_name: str, request: Request, session: AsyncSession = Depends(get_session)):
     detail_model = DetailPackageViewModel(package_name, request)
-    await detail_model.load()
+    await detail_model.load(session=session)
     return detail_model.to_dict()
